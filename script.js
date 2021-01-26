1ff06c87d609f7b0fe0849442065b1e4b65996d4
Stepa <nizarincorp@gmail.com>
2021-01-26 12:05:12 +0300

    Ni4ego

--- script.js
+++ script.js
@@ -1,56 +1,160 @@
-class Contacts {
-   constructor() {
-       this.data = {
-           list: []
-       }
-   }
-
-   add = () => {
-
-   }
-
-   edit = (id, obj) => {
-
-   }
-   remove = (id) => {
-
-   }
-
-   get = () => {
-       return this.data;
-   }
-}
-
 class User {
-    constructor() {
+    constructor(name, email, address, phone) {
         this.data = {
-            id: '',
-            name: '',
-            email: '',
-            address: '',
-            phone: ''
+            name: name,
+            email: email,
+            address: address,
+            phone: phone
         }
     }
 
-    edit = (obj) => {
-        this.data.id = obj.id
-        this.data.name = obj.name
-        this.data.email = obj.email
-        this.data.address = obj.address
-        this.data.phone = obj.phone
-    }
-
-    get = () => {
+    get () {
         return this.data;
     }
 }
 
-class ContactsApp extends Contacts {
-    
+
+class Contacts {
+   constructor() {
+       this.users =  []
+   }
+
+   add (name, email, address, phone) {
+    const user = new User (name, email, address, phone)
+    const myUser = user.get()
+    this.users.push({id: this.users.lenght, ...myUser})
+
+   }
+
+   edit (id, name, email, address, phone) {
+    this.users[id].name = name;
+    this.users[id].email = email;
+    this.users[id].address = address;
+    this.users[id].phone = phone;
+   }
+   remove (id) {
+    delete this.users[id]
+   }
+
+   get () {
+       return this.users;
+   }
 }
 
 
-let stalin = new User;
 
-stalin.edit({id: '1', name: 'Stalin', email:'ussr@world.ru', address: 'USSR', phone:'+1-111'})
-console.log(stalin.get());
+class ContactsApp extends Contacts {
+    constructor() {
+        super() //для добавления новых методов
+    }
+
+    display() {
+        const self = this
+
+        const form = document.createElement('form')
+        form.innerHTML = `
+            <input type = 'text'>
+            <input type = 'text'>
+            <input type = 'text'>
+            <input type = 'text'>
+            <button type = 'submit'> Add </button>
+        `
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault()  //запрет обновления страницы
+            const name = event.currentTarget[0].value
+            const email = event.currentTarget[1].value
+            const address = event.currentTarget[2].value
+            const phone = event.currentTarget[3].value
+
+            event.currentTarget[0].value = event.currentTarget[1].value = event.currentTarget[2].value = event.currentTarget[3].value = ''
+            self.add(name, email, address, phone)
+        
+           
+
+            this.displayContacts()
+        })
+
+        document.body.appendChild(form)
+    }
+
+    editModalWimdow(contactID, userData) {
+        const {id, name, email, address, phone} = userData
+
+        const editBlock = document.getElementById(contactID)
+
+        const editForm = document.createElement('div')
+        const inputName = document.createElement('input')
+        const inputEmail = document.createElement('input')
+        const inputAddress = document.createElement('input')
+        const inputPhone = document.createElement('input')
+        const save = document.createElement('button')
+        save.innerHTML = 'Save'
+
+        inputName.value = name
+        inputEmail.value = email
+        inputAddress.value = address
+        inputPhone.value = phone
+
+        editForm.appendChild(inputName)
+        editForm.appendChild(inputEmail)
+        editForm.appendChild(inputAddress)
+        editForm.appendChild(inputPhone)
+        editForm.appendChild(save)
+
+        save.addEventListener('click', (event) => {
+
+            this.displayContacts()
+        })
+
+        document.getElementById(contactID).appendChild(editForm)
+    }
+
+    
+
+    displayContacts() {
+        const users = this.get
+        const self = this
+
+        if (document.getElementById('contactsBlock')){
+            document.getElementById('contactsBlock').remove()
+        }
+
+        const contactsBlock = document.createElement('div')
+        contactsBlock.id = 'contactsBlock'
+        self.users.map(user => {
+            const contact = document.createElement('div')
+            const CONTACT_ID = user.name + '_' + user.id
+            contact.id = CONTACT_ID
+            contact.innerHTML = `
+                Name: ${user.name} <br>
+                Email: ${user.email} <br>
+                Address: ${user.address} <br>
+                Phone: ${user.phone} <br>
+            `;
+            const remove = document.createElement('button');
+            remove.innerHTML = 'Delete'
+            remove.addEventListener('click', () => {
+                self.remove(user.id)
+                self.displayContacts()
+            })
+            contact.appendChild(remove)
+
+            const edit = document.createElement('button')
+            edit.innerHTML = 'Edit'
+            edit.addEventListener ('click', () => {
+                this.editModalWimdow(CONTACT_ID, user)
+                
+            })
+            contact.appendChild(edit)
+
+            contactsBlock.appendChild(contact)
+        })
+        document.body.appendChild(contactsBlock)
+    }
+
+}
+
+const app = new ContactsApp()
+app.display()
+app.displayContacts()
